47e8101d904f2fa176620a55adce835279bde28e
Hiraku NAKANO <hiraku@tojiru.net>
2016-11-28 02:08:04 +0900

    add RuntimeMaker class

---
+++ src/RuntimeMaker.php
@@ -0,0 +1,101 @@
+<?php
+namespace Hirak\Kibi;
+
+class RuntimeMaker
+{
+    private $namespaces = [];
+    private $traits = [];
+    private $factories = [];
+
+    public function addNamespace(string $namespace)
+    {
+        $this->namespaces[$namespace] = true;
+    }
+
+    public function addTraits(string $trait)
+    {
+        $this->traits[$trait] = true;
+    }
+
+    /**
+     * @param $namespace ex: Acme\Special
+     * @param $type      ex: string, int, Acme\Special\Klass
+     * @param $name      exclude 'get' prefix
+     * @param $trait
+     * @param object $instance
+     */
+    public function addFactory(string $namespace, string $type, string $name, string $trait, $instance)
+    {
+        $key = strtr($namespace, '\\', '_') . '__' . strtr($type, '\\', '_') . '__' . $name;
+        $count = 0;
+        // すでに登録されている場合は何もしない
+        if (!isset($this->factories[$key])) {
+            $this->factories[$key] = compact('instance', 'trait', 'name', 'count');
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function generate(string $namespace, string $type, string $name)
+    {
+        $normalizedtype = strtr($type, '\\', '_');
+        $keys = [];
+        foreach ($this->namespaces as $ns) {
+            if (0 === strncmp($ns, $namespace, strlen($ns))) {
+                $normalizedns = strtr($ns, '\\', '_');
+                $keys[] = "{$normalizedns}__{$normalizedtype}__{$name}";
+                $keys[] = "{$normalizedns}__{$normalizedtype}__";
+                $keys[] = "{$normalizedns}____{$name}";
+            }
+        }
+        $keys[] = "__{$normalizedtype}__{$name}";
+        $keys[] = "__{$normalizedtype}__";
+        $keys[] = "____{$name}";
+        
+        foreach ($keys as $key) {
+            if (isset($this->factories[$key])) {
+                $factory = $this->factories[$key];
+                $response = $factory['instance']->{'get' . ucfirst($factory['name'])}();
+
+                if (0 === $factory['count']) {
+                    // 返却できるものが他にあるとしたらそれも追加しておく
+                    if (is_object($return)) {
+                        foreach (class_implements($return) as $interface) {
+                            $this->addFactory($namespace, $interface, $name, $factory['trait'], $factory['instance']);
+                        }
+                        foreach (class_parents($return) as $class) {
+                            $this->addFactory($namespace, $class, $name, $factory['trait'], $factory['instance']);
+                        }
+                    }
+                    ++$factory['count'];
+                }
+                return $response;
+            }
+        }
+
+        // ここから先はルールが見つからなかったときの処理
+        if (!class_exists($type)) {
+            // typeがクラス名じゃないならお手上げ
+            throw new Exception\CannotInstantiateError("$type cannot instantiate.");
+        }
+
+        // クラスの場合は、とりあえずそのクラスをインスタンス化しようとしてみる
+        $rc = new ReflectionClass($type);
+        $constr = $rc->getConstructor();
+        if (!$constr) {
+            return $rc->newInstance();
+        }
+
+        $params = [];
+        foreach ($constr->getParameters() as $param) {
+            if ($param->hasType()) {
+                $params[] = $this->generate($namespace, (string)$param->getType(), (string)$param);
+            } else {
+                $params[] = $this->generate($namespace, '', (string)$param);
+            }
+        }
+
+        return $rc->newInstanceArgs($params);
+    }
+}
